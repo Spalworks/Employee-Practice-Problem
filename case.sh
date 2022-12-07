64235d5e0c083259021ff66316fce53f057035ba
spalworks <spalworks@gmail.com>
2022-12-08 00:55:40 +0530

    [Sourav]-Employee practice committed successfully

---
+++ case.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for employeePracticeProblem in $(ls)
+do
+	ext=${employeePracticeProblem##*\.}
+	case "$ext" in
+		java) echo "$employeePracticeProblem : Java source file"
+			;;
+		o)    echo "$employeePracticeProblem : Object file"
+			;;
+		sh)   echo "$employeePracticeProblem : Shell Script file"
+			;;
+		txt)  echo "$employeePracticeProblem: Text file"
+			;;
+		*)    echo "$employeePracticeProblem : Not Processed"
+	esac
+done
